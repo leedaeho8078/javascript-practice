8de79be288d3fcca6ef0fca42a51af75a13f29d3
Lee Daeho <dooho199234@nate.com>
2021-06-29 17:01:02 +0900

    Template Literal

---
+++ Template_Literal.js
@@ -0,0 +1,17 @@
+//Template literals
+
+function hello(name) {
+  console.log("Hello" + name + ". Welcome!");
+}
+
+function hello2(name, name2) {
+  console.log(`hellO ${name}  .Welcome! ${name2}`);
+}
+
+// hello2("leedaeho");
+
+function print(name, age) {
+  console.log(`hello ${name} welcome, 내 나이는 ${age}`);
+}
+
+print("이대호", 14);
